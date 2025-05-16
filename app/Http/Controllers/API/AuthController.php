35223d7c33af19239f81ef4831bc9704b42328c8
Alexey Parkhomenko <aleksejparhomenko14192@gmail.com>
2025-05-16 13:32:29 +0300

    add basic auth functions

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -5,16 +5,58 @@ namespace App\Http\Controllers\API;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
 
 use App\Models\User;
+use App\Http\Resources\UserResource;
 
 class AuthController extends Controller
 {
-    public function login(Request $request) {
+    public function register(Request $request) {
+        $data = $request->validate([
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:8|confirmed'
+        ]);
 
+        $user = User::create([
+            'email' => $data['email'],
+            'password'=> Hash::make($data['password']),
+        ]);
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            'token'=> $token,
+            'message' => 'User registered successfully'
+        ], 201);
     }
 
-    public function register(Request $request) {
+    public function login(Request $request) {
+        $data = $request->validate([
+            'email' => 'required|string|email',
+            'password' => 'required|string',
+        ]);
+        
+        $user = User::where('email', $request->email)->first();
+       
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            return ValidationException::withMessages(['email' => ['Invalid credentials']]);
+        }
 
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            'token' => $token,
+            'message' => 'User logged in successfully',
+            'user' => new UserResource($user),
+        ]);
+    } 
+
+    public function logout(Request $request) {
+        $request->user()->tokens()->delete();
+
+        return response()->json([
+            'message' => 'Success logout'
+        ]);
     }
 }
